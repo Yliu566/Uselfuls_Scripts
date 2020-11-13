c8e6296d54dfa57d7afb9a67803c228368741e36
Yuru Liu <32871756+Yliu566@users.noreply.github.com>
2020-11-13 15:13:15 +0100

    convert str to float in the list, add symbols for the plot

--- anayze_ORCA.py
+++ anayze_ORCA.py
@@ -75,7 +75,26 @@ for line in lines:
 		Change_1st_SCF_cycle.append(lines[counter+2].split()[3])
 # ------------------------------------------ #
 
+#----------- str to float ------------------------- #
 
+Energy=[float(i) for i in Energy]
+E_change=[float(i) for i in E_change]
+E_threshold=[float(i) for i in E_threshold]
+MaxGrad=[float(i) for i in MaxGrad]
+MaxGrad_threshold=[float(i) for i in MaxGrad_threshold]
+MaxStep=[float(i) for i in MaxStep]
+MaxStep_threshold=[float(i) for i in MaxStep_threshold]
+RMSGrad=[float(i) for i in RMSGrad]
+RMSGrad_threshold=[float(i) for i in RMSGrad_threshold]
+RMSStep=[float(i) for i in RMSStep]
+RMSStep_threshold=[float(i) for i in RMSStep_threshold]
+E_1st_SCF_cycle=[float(i) for i in E_1st_SCF_cycle]
+Change_1st_SCF_cycle=[float(i) for i in Change_1st_SCF_cycle]
+
+# print(Energy)
+# print(E_change)
+# print(E_threshold)
+#--------------------------------------------#
 
 # ------------- PLOTTING STUFF ------------- #			
 # If only SCF cycle, plot OPTION 1.
@@ -92,11 +111,11 @@ if E_change == []:
 	ax = plt.subplot(211)	
 	plt.title("1st SCF CYCLE Energy")
 	ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-	ax.plot(list(range(1, len(E_1st_SCF_cycle)+1)), E_1st_SCF_cycle, 'r-')
+	ax.plot(list(range(1, len(E_1st_SCF_cycle)+1)), E_1st_SCF_cycle, 'ro-')
 	
 	plt.subplot(212)
 	plt.title("1st SCF CYCLE Energy Change")
-	plt.plot(list(range(1, len(Change_1st_SCF_cycle)+1)), Change_1st_SCF_cycle, 'b-')
+	plt.plot(list(range(1, len(Change_1st_SCF_cycle)+1)), Change_1st_SCF_cycle, 'bo-')
 
 
 # OPTION 2:
@@ -104,28 +123,28 @@ else:
 	plt.figure(figsize=(20,10))
 	plt.subplot(321)
 	plt.title("Energy Change")
-	plt.plot(list(range(1, len(E_change)+1)), E_change, 'r--', list(range(1, len(E_threshold)+1)), E_threshold, 'b--')
+	plt.plot(list(range(1, len(E_change)+1)), E_change, 'ro--', list(range(1, len(E_threshold)+1)), E_threshold, 'bo--')
 
 	ax = plt.subplot(322)
 	plt.title("SCF Energy")
 	ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-	ax.plot(range(len(Energy)), Energy, 'ro')
+	ax.plot(range(len(Energy)), Energy, 'ro--')
 
 	plt.subplot(323)
 	plt.title("Max Gradient")
-	plt.plot(range(len(MaxGrad)), MaxGrad, 'g--', range(len(MaxGrad_threshold)), MaxGrad_threshold, 'b--')
+	plt.plot(range(len(MaxGrad)), MaxGrad, 'go--', range(len(MaxGrad_threshold)), MaxGrad_threshold, 'bo--')
 
 	plt.subplot(324)
 	plt.title("Max Step")
-	plt.plot(range(len(MaxStep)), MaxStep, 'go', range(len(MaxStep_threshold)), MaxStep_threshold, 'b--')
+	plt.plot(range(len(MaxStep)), MaxStep, 'go--', range(len(MaxStep_threshold)), MaxStep_threshold, 'bo--')
 
 	plt.subplot(325)
 	plt.title("RMS Gradient")
-	plt.plot(range(len(RMSGrad)), RMSGrad, 'y--', range(len(RMSGrad_threshold)), RMSGrad_threshold, 'b--')
+	plt.plot(range(len(RMSGrad)), RMSGrad, 'yo--', range(len(RMSGrad_threshold)), RMSGrad_threshold, 'bo--')
 
 	plt.subplot(326)
 	plt.title("RMS Gradient")
-	plt.plot(range(len(RMSStep)), RMSStep, 'yo', range(len(RMSStep_threshold)), RMSStep_threshold, 'b--')
+	plt.plot(range(len(RMSStep)), RMSStep, 'yo--', range(len(RMSStep_threshold)), RMSStep_threshold, 'bo--')
 
 plt.show()
 
